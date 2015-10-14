8b1eafed91e9070d978e73a5e1acf8f4098100af
Igor <igor.poplava@gmail.com>
2015-10-14 22:55:29 +0300

    Games components

--- client/containers/GamePage.js
+++ client/containers/GamePage.js
@@ -1,12 +1,36 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
+import Form from '../components/games/Form';
+
+import {
+  formChange,
+  formSetParams,
+  formSubmit,
+  formMore
+} from '../actions/GamesActions';
+
 class GamePage extends Component {
+  componentDidMount() {
+    this.props.formSetParams(this.props.query);
+  }
+
   render() {
+    const { form, formSubmit, formChange } = this.props;
+
     return (
-      <div className="row">
-        <div className="col-md-12">
-          GamePage
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-3">
+            <Form
+              params={form.params}
+              formSubmit={formSubmit}
+              formChange={formChange}
+              />
+          </div>
+          <div className="col-md-9">
+            Game List
+          </div>
         </div>
       </div>
     );
@@ -14,10 +38,31 @@ class GamePage extends Component {
 }
 
 GamePage.propTypes = {
+  form: PropTypes.object.isRequired,
+  list: PropTypes.object.isRequired,
+  query: PropTypes.object.isRequired,
+  formSubmit: PropTypes.func.isRequired,
+  formChange: PropTypes.func.isRequired,
+  formSetParams: PropTypes.func.isRequired,
+  formMore: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state) {
-  return {};
+  const { games } = state;
+
+  return {
+    form: games.form.toJS(),
+    list: {
+      status: games.list.get('status'),
+      items: games.list.get('items').toList().toJS()
+    },
+    query: state.router.location.query
+  };
 }
 
-export default connect(mapStateToProps)(GamePage);
+export default connect(mapStateToProps, {
+  formChange,
+  formSetParams,
+  formSubmit,
+  formMore
+})(GamePage);
