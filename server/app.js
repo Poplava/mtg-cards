1087277eeb887cb6f9465246981c7cb14c0a0ca6
Igor <igor.poplava@gmail.com>
2015-10-23 21:23:12 +0300

    added initial user data to res body

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import express from 'express';
 import path from 'path';
 
@@ -13,7 +14,13 @@ init.call(app);
 router.call(app);
 
 app.use((req, res) => {
-  res.sendFile(path.normalize(path.join(__dirname, '../client/index.html')));
+  fs.readFile(path.normalize(path.join(__dirname, '../client/index.html')), 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json(err.message);
+    }
+
+    return res.send(data.replace('#{INITIAL_STATE}', JSON.stringify({ user: req.user || null })));
+  });
 });
 
 app.listen(SERVER_PORT);
--- server/app/models/User.js
+++ server/app/models/User.js
@@ -5,7 +5,7 @@ const schema = new Schema({
   displayName: String,
   email: String,
   photo: String,
-  role: { type: Array, default: [] }
+  roles: { type: Array, default: [] }
 });
 
 schema.statics.authGoogle = function(profile, done) {
