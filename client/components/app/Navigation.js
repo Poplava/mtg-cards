1d375e4d93c54b4eacd28133bd6224cdd1d368fb
Igor <igor.poplava@gmail.com>
2015-10-27 00:01:25 +0200

    new theme; navigation fi

--- client/components/app/Navigation.js
+++ client/components/app/Navigation.js
@@ -1,12 +1,14 @@
 import React, { Component, PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
 
+import './less/navigation.less';
+
 class Navigation extends Component {
   render() {
     const { user } = this.props;
 
     return (
-      <nav className="navbar navbar-default">
+      <nav className="navbar navbar-default Nav__bar">
         <div className="container-fluid">
           <div className="navbar-header">
             <IndexLink to="/" className="navbar-brand">MTG Cards Game</IndexLink>
