3978b33d5825ec072ba11b890e23854fc359008f
Igor <igor.poplava@gmail.com>
2015-10-31 19:07:17 +0200

    returned permissions

--- server/modules/game/index.js
+++ server/modules/game/index.js
@@ -5,5 +5,5 @@ import * as UserController from '../user/UserController';
 
 export const router = Router();
 
-router.get('/', /* UserController.ensureUser,*/ GameController.list);
-router.put('/', /* UserController.ensureAdmin,*/ GameController.put);
+router.get('/', UserController.ensureUser, GameController.list);
+router.put('/', UserController.ensureAdmin, GameController.put);
